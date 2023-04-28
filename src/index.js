b5bd4588cae532c4f99f6715128451b27daa3722
BenPerraud <benjaminperraud@outlook.fr>
2023-04-28 17:21:00 +0200

    version_404_ok

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import * as React from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import './styles/index.css'
 import Accueil from "./pages/Accueil"
 import Propos from "./pages/Propos"
 import Housing from "./pages/Housing"
 import Banner from "./components/Banner"
 import Footer from "./components/Footer"
+import ErrorBoundary from "./pages/Error"
 
 const root = createRoot(document.getElementById("root"));
 
@@ -18,6 +19,7 @@ root.render(
         <Route path="/" element={<Accueil />} />
         <Route path="/a-propos" element={<Propos />} />
         <Route path="/Housing" element={<Housing />} />
+        <Route path="*" element={<ErrorBoundary />} /> 
       </Routes>
     <Footer />
   </BrowserRouter>
---
+++ src/pages/Error/index.js
@@ -0,0 +1,15 @@
+import "./error.css"
+import {Link, useRouteError} from "react-router-dom"
+
+function ErrorBoundary () {
+    return (
+        <div className="error">
+            <span className="error404">404</span>
+            <p className="error404-text">Oups! La page que vous demandez n'existe pas</p>
+            <Link className="error404-redirect" to="/">Retourner sur la page d'acceuil</Link>
+        </div>
+    )
+}
+
+
+export default ErrorBoundary
