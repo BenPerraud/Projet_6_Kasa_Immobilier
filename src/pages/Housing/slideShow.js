f5d720feae3ce5bf69c43d4c04d41702b3ebecb5
BenPerraud <benjaminperraud@outlook.fr>
2023-05-10 17:44:13 +0200

    responsive_to_do

--- src/pages/Housing/slideShow.js
+++ src/pages/Housing/slideShow.js
@@ -18,11 +18,20 @@ function SlideShow ({z}) {
         setCount(IndexDown(count, z))
     }
 
+    function picturesIndex(z) {
+        if (z.pictures.length > 1) {
+            return true
+        } else {
+            return false
+        }
+    }
+
     return (
         <div>
             <div className="carrousel">
-                <div className="chevron-position">
+                <div className={picturesIndex(z) ? "index" : "hidden"}>
                     <FontAwesomeIcon onClick={clickHandlerDown} className="carrousel-chevron" icon={faChevronLeft}></FontAwesomeIcon>
+                    <div className="index-count">{count+1}/{z.pictures.length}</div>
                     <FontAwesomeIcon onClick={clickHandlerUp} className="carrousel-chevron" icon={faChevronRight}></FontAwesomeIcon>
                 </div>
                 <img className="carrousel-img" src={z.pictures[count]} alt="Carrousel des photos du logement"></img>
